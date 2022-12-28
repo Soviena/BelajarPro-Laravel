9e9bdcbef71e9a8590c4d65c73861984835fb8d1
Soviena <rovino.rs@gmail.com>
2022-12-28 14:34:32 +0700

    update komunitaas dan routing

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,18 @@ use App\Http\Controllers\adminController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home')->name('dashboard');
 
+// KOMUNITAS
 Route::get('/komunitas', [komunitasController::class, 'index'])->name('komunitas');
+Route::post('/komunitas/post/add', [komunitasController::class, 'addPost'])->name('addPost');
+Route::post('/komunitas/comment/add', [komunitasController::class, 'addComment'])->name('addComment');
+
+
 Route::get('/course', [CourseController::class, 'index'])->name('course');
 Route::get('/admin/course', [adminController::class, 'course'])->name('admin-course');
 Route::get('/admin/user', [adminController::class, 'user'])->name('admin-user');
-Route::post('/admin/user', [adminController::class, 'edit_user'])->name('edit-user');
-Route::get('/admin/user/{parameter}', [adminController::class, 'delete_user'])->name('delete-user');
-Route::get('/masuk', [adminController::class, 'delete_user'])->name('delete-user');
+Route::post('/admin/edit/user', [adminController::class, 'edit_user'])->name('edit-user');
+Route::get('/admin/user/delete/{parameter}', [adminController::class, 'delete_user'])->name('delete-user');
+// Route::get('/masuk', [adminController::class, 'delete_user'])->name('delete-user');
 
