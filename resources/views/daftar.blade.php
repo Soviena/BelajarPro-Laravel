fd1b102a676b884c2c2fa172349b88724ced9cd0
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2023-01-10 23:17:13 +0700

    Menerapkan Form validation Penambahan form validation pada file terkait berikut >userController, penambahan berupa pengecekan email dan password, jika salah akan di redirect atau me-return view dengan flashdata/compact() yang berisi pesan kesalahan dan email atau password yang dimasukkan +addUser() +masuk()
    >view daftar dan masuk
    +menambahkan regex
    +pengkondisian untuk mengecek ada tidaknya data session dan pesan kesalahan
    +script js untuk show/hide password
    
    >layout.blade
    +menambahkan referensi css/js untuk icon eye,
    (mungkin bisa dihapus, referensi link dsb blm disesuaikan!)

--- resources/views/daftar.blade.php
+++ resources/views/daftar.blade.php
@@ -8,7 +8,16 @@
                 <h1 class="mt-3 text-center">Daftar</h1>
                 <div class="ms-3 mb-3 me-3">
                     <label for="email" class="form-label">Alamat Email</label>
-                    <input type="email" class="form-control" id="email" name="email" aria-describedby="emailHelp" placeholder="email@email.com" required>
+                    <input type="email" class="form-control @if(session('daftar-failed')) <?= "is-invalid" ?> @endif)" id="email" name="email" aria-describedby="emailHelp" placeholder="nama@gmail.com" required value="
+                    <?php
+                    if (session('daftar-failed')) {
+                        echo session('daftar-failed.email');
+                    }
+                    ?>" pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?">
+
+                    <div class="invalid-feedback">
+                        {{session('daftar-failed.msg1')}}
+                    </div>
                 </div>
                 <div class="mb-3 ms-3 me-3">
                     <label for="name" class="form-label">Nama Pengguna</label>
@@ -16,14 +25,23 @@
                 </div>
                 <div class="mb-3 ms-3 me-3">
                     <label for="password" class="form-label">Kata Sandi</label>
-                    <input type="password" class="form-control" id="password" name="password" required>
+                    <div class="input-group" id="show_hide_password">
+                        <input type="password" class="form-control @if(session('daftar-failed')) <?= "is-invalid" ?> @endif)" id="password" name="password" required value="@if (session('daftar-failed.pw'))<?=session('daftar-failed.pw');?>@endif" pattern="^(?=\S*\d)(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[a-zA-Z])\S{7,}\S$">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="bi bi-eye-slash" aria-hidden="true"></i></button>
+                    </div>
+                    <div id="passwordHelpBlock" class="form-text">
+                        Panjang Password harus 8-15 karakter, tidak mengandung spasi, karakter spesial,
+                        atau emoji.
+                    </div>
                 </div>
                 <div class="mb-3 ms-3 me-3">
                     <label for="password2" class="form-label">Konfirmasi Kata Sandi</label>
-                    <input type="password" name="password2" class="form-control" required>
-                    <div id="passwordHelpBlock" class="form-text">
-                    Panjang Password harus 8-15 karakter, tidak mengandung spasi, karakter spesial,
-                        atau emoji.
+                    <div class="input-group" id="show_hide_password">
+                        <input type="password" name="password2" class="form-control @if(session('daftar-failed')) <?= "is-invalid" ?> @endif)" required pattern="^(?=\S*\d)(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[a-zA-Z])\S{7,}\S$">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="bi bi-eye-slash" aria-hidden="true"></i></button>
+                        <div class="invalid-feedback">
+                            {{session('daftar-failed.msg2')}}
+                        </div>
                     </div>
                 </div>
                 <div class="text-center mb-3 ms-3 me-3">
@@ -33,4 +51,20 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $("#show_hide_password button").on('click', function(event) {
+            event.preventDefault();
+            if ($('#show_hide_password input').attr("type") == "text") {
+                $('#show_hide_password input').attr('type', 'password');
+                $('#show_hide_password i').addClass("bi bi-eye-slash");
+                $('#show_hide_password i').removeClass("bi bi-eye");
+            } else if ($('#show_hide_password input').attr("type") == "password") {
+                $('#show_hide_password input').attr('type', 'text');
+                $('#show_hide_password i').removeClass("bi bi-eye-slash");
+                $('#show_hide_password i').addClass("bi bi-eye");
+            }
+        });
+    });
+</script>
 @endsection
--- resources/views/masuk.blade.php
+++ resources/views/masuk.blade.php
@@ -2,27 +2,35 @@
 @section('content')
 
 <div class="container">
-<?php
+    <?php
     // error_log($data['remember']);
     // error_log("22");
-?>
+    ?>
     <div class="row justify-content-center border">
         <div class="col-4 mt-5 border border-success">
             <form class="form-floating needs-validation" action="{{route('submit')}}" method="POST">
-            @csrf
+                @csrf
                 <h1 class="mt-3 text-center">Masuk</h1>
                 <div class="mb-3 ms-3 me-3">
                     <label for="email" class="form-label">Alamat Email</label>
-                    <input type="email" class="form-control" id="email" name="email" aria-describedby="email" required 
-                        value="<?php if ($data["remember"]){ echo $data["email"];}?>">
+                    <input type="email" class="form-control @if(session('login-failed')) <?= "is-invalid" ?> @endif)" id="email" name="email" aria-describedby="email" required placeholder="nama@gmail.com" 
+                    value="@if($data['remember'])<?=$data['email']?>@endif @if(session('login-failed'))<?=session('login-failed.email')?>@endif" pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?">
+                    <div class="invalid-feedback">
+                        {{session('login-failed.msg1')}}
+                    </div>
                 </div>
+                
                 <div class="mb-3 ms-3 me-3">
                     <label for="password" class="form-label">Kata Sandi</label>
-                    <input type="password" class="form-control" id="password" name="password" required
-                        value="<?php if ($data["remember"]){echo $data["password"];}?>">
-
+                    <div class="input-group" id="show_hide_password">
+                        <input type="password" class="form-control @if(session('login-failed')) <?= "is-invalid" ?> @endif" id="password" name="password" required value="@if($data['remember'])<?=$data['password'];?>@endif" pattern="^(?=\S*\d)(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[a-zA-Z])\S{7,}\S$">
+                        <button class="btn btn-outline-secondary" type="button" id="button-addon1"><i class="bi bi-eye-slash" aria-hidden="true"></i></button>
+                        <div class="invalid-feedback">
+                            {{session('login-failed.msg2')}}
+                        </div>
+                    </div>
                     <div class="form-text">
-                        Panjang Password harus 8-15 karakter, tidak mengandung spasi, karakter spesial,
+                        Password harus mengandung kapital dan angka dengan panjang 8-15 karakter, tidak mengandung spasi, karakter spesial,
                         atau emoji.
                     </div>
                 </div>
@@ -42,4 +50,20 @@
         </div>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $("#show_hide_password button").on('click', function(event) {
+            event.preventDefault();
+            if ($('#show_hide_password input').attr("type") == "text") {
+                $('#show_hide_password input').attr('type', 'password');
+                $('#show_hide_password i').addClass("bi bi-eye-slash");
+                $('#show_hide_password i').removeClass("bi bi-eye");
+            } else if ($('#show_hide_password input').attr("type") == "password") {
+                $('#show_hide_password input').attr('type', 'text');
+                $('#show_hide_password i').removeClass("bi bi-eye-slash");
+                $('#show_hide_password i').addClass("bi bi-eye");
+            }
+        });
+    });
+</script>
 @endsection
