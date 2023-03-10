0c3bf3113a79ba0d1855ba02a83fa26df1090c49
RizqiAnshari <muhammadrizqia@student.telkomuniversity.ac.id>
2023-03-10 19:52:52 +0700

    + menerapkan tema warna pada halaman login + menambahkan file css pada folder public dengan nama style.css

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -5,8 +5,10 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+        <link rel="stylesheet" href="{{ asset('style.css') }}">
         <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
         <script src="{{ asset('js/jquery-3.6.1.min.js') }}"></script>
+        <!-- D:\Laravel\Project\belajarpro-laravel\resources\css\app.css -->
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <!-- <link rel="stylesheet" href="../../public/bootstrap/css/bootstrap.min.css"> -->
         <!-- <script src="bootstrap-5.2.2-dist/js/bootstrap.bundle.min.js"></script> -->
@@ -16,7 +18,6 @@
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-
     </head>
 
     <body>
