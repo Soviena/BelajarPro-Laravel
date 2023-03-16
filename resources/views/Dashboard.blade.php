a0b6a0721ee1b9a439784e50e04f0b3e7dbe184b
debinov <debinv4nt@gmail.com>
2023-03-16 11:42:49 +0700

    penerapan color palette pada card

--- resources/views/Dashboard.blade.php
+++ resources/views/Dashboard.blade.php
@@ -3,32 +3,77 @@
 
         @if (session('loggedin',FALSE))
             <section id="dashboard">
-                <div class="container">
-                    <div class="row">
-                        <h1 class="text-info fw-bold" style="text-align: center; ">
-                            Selamat Datang Di Website Belajar Pro
-                        </h1>
-                            <div class="container-fluid">
-                                <div class="row">
-                                <div class="col "  style="text-align: center;">
-                                    Ingin belajar codingan tapi dapat referensi bahasa Inggris semua, yuk belajar disini aja.
-                                    Di Website ini aja, disini anda dapat menemukan referensi dengan bahasa Indonesia 
-                                    dan jika kamu bingung disini menyediakan  beberapa fitur, seperti komunitas diskusi dan mentoring
-                                </div>
-                                </div>
+                <div class="container row-dash-bel">
+                    <div class="card mb-3">
+                            <div class="card-body ">
+                                <h1 class="text-info fw-bold" style="text-align: center; ">
+                                    Selamat Datang Di Website BelajarPro
+                                </h1>
+                                    <p class="card-text fs-5" style="text-align: center;">
+                                        Ingin belajar codingan tapi dapat referensi bahasa Inggris semua, yuk belajar disini aja.
+                                        Di Website ini aja, disini anda dapat menemukan referensi dengan bahasa Indonesia 
+                                        dan jika kamu bingung disini menyediakan  beberapa fitur, seperti komunitas diskusi dan mentoring
+                                    </p>
                             </div>
-                    </div>        
+                    </div>
                     
                     <br>
 
                     <div class="row row-cols-1 row-cols-md-2 g-4">
                     @foreach ($courses as $c)
                         <div class="col">
-                        <div class="card text-white bg-info" >
+                            <div class="card text-white card-dash-bel" >
+                                <img src="{{asset('storage/uploaded/Course/'.$c->img)}}"  style="height:20vh; object-fit: cover; object-position: 25% 25%;" class="card-img-top" alt="Pemroragrman HTML">
+                                <div class="card-body">
+                                    <h2 class="card-dash-bel fw-bold">{{$c->name}}</h2>   
+                                    <p class="card-dash-bel fs-5">
+                                        {{$c->deskripsi}}
+                                    </p>
+                                <div class="text-center">
+                                    <a href="{{route('article',$c->id)}}" class="btn btn-light">Pelajari</a>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                        <div class="col">
+                        <div class="card text-white card-dash-bel" >
                             <img src="{{asset('storage/uploaded/Course/'.$c->img)}}"  style="height:20vh; object-fit: cover; object-position: 25% 25%;" class="card-img-top" alt="Pemroragrman HTML">
                             <div class="card-body">
-                                <h2 class="card-title fw-bold">{{$c->name}}</h2>   
-                                <p class="card-text fs-5">
+                                <h2 class="card-dash-bel fw-bold">{{$c->name}}</h2>   
+                                <p class="card-dash-bel fs-5">
+                                    {{$c->deskripsi}}
+                                </p>
+                            <div class="text-center">
+                                <a href="{{route('article',$c->id)}}" class="btn btn-light">Pelajari</a>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+
+                        <div class="col">
+                        <div class="card text-white  card-dash-bel" >
+                            <img src="{{asset('storage/uploaded/Course/'.$c->img)}}"  style="height:20vh; object-fit: cover; object-position: 25% 25%;" class="card-img-top" alt="Pemroragrman HTML">
+                            <div class="card-body">
+                                <h2 class="card-dash-bel fw-bold">{{$c->name}}</h2>   
+                                <p class="card-dash-bel fs-5">
+                                    {{$c->deskripsi}}
+                                </p>
+                            <div class="text-center">
+                                <a href="{{route('article',$c->id)}}" class="btn btn-light">Pelajari</a>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+
+
+                        <div class="col">
+                        <div class="card text-white  card-dash-bel" >
+                            <img src="{{asset('storage/uploaded/Course/'.$c->img)}}"  style="height:20vh; object-fit: cover; object-position: 25% 25%;" class="card-img-top" alt="Pemroragrman HTML">
+                            <div class="card-body">
+                                <h2 class="card-dash-bel fw-bold">{{$c->name}}</h2>   
+                                <p class="card-dash-bel fs-5">
                                     {{$c->deskripsi}}
                                 </p>
                             <div class="text-center">
@@ -48,7 +93,7 @@
 
         @else
             <section id="dashboard_landing">
-                <div class="container">
+                <div class="container row-bel">
                     <div class="card mb-3">
                         <img src="{{asset('storage/assets/programmer.jpg')}}" class="card-img-top" alt="programmer">
                             <div class="card-body">
