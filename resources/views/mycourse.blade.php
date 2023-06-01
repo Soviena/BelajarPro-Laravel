c64dcfda2f206cbff437c2c2d46058595bd91a31
Debi Ratnasari Novianti <debinv4nt@gmail.com>
2023-06-01 13:02:57 +0700

    update halaman kursusku

--- resources/views/mycourse.blade.php
+++ resources/views/mycourse.blade.php
@@ -13,32 +13,32 @@
         </div>
             
         <div class="container table-bel">
-    <table class="table table-light table-bordered table-striped">
-        <thead>
-            <tr>
-                <th scope="col" style="text-align: center;">ID Course</th>
-                <th scope="col">Nama Course</th>
-                <th scope="col" style="text-align: center;">Deskripsi</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($mycourses as $c)
-            <tr>
-                <td>{{ $c->id }}</td>
-                <td>{{ $c->name }}</td>
-                <td>{{ $c->deskripsi }}</td>
-                <td>
-                    <div class="d-flex justify-content-between">
-                        <button type="button" class="btn btn-primary mx-2" data-bs-toggle="modal" data-bs-target="#edit-{{$c->id}}">Lihat</button>
-                        <button type="button" class="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#edit-{{$c->id}}">Hapus</button>
-                    </div>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+            <table class="table table-light table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col" style="text-align: center;">ID Course</th>
+                        <th scope="col">Nama Course</th>
+                        <th scope="col" style="text-align: center;">Deskripsi</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($mycourses as $c)
+                    <tr>
+                        <td>{{ $c->id }}</td>
+                        <td>{{ $c->name }}</td>
+                        <td>{{ $c->deskripsi }}</td>
+                        <td>
+                            <div class="d-flex justify-content-between">
+                                <button type="button" class="btn btn-primary mx-2" data-bs-toggle="modal" data-bs-target="#edit-{{$c->id}}">Lihat</button>
+                                <button type="button" class="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#edit-{{$c->id}}">Hapus</button>
+                            </div>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
 
    
 
