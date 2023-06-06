52a5cf06d7b998ff0569d2a23e7db83c9ee4f5c3
Muhammad Rovino Sanjaya <rovino.rs@gmail.com>
2023-06-06 09:36:07 +0700

    add login api

--- app/Http/Controllers/apiController.php
+++ app/Http/Controllers/apiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\course;
 use App\Models\article;
+use Illuminate\Support\Facades\DB;
 
 
 
@@ -18,5 +19,33 @@ class apiController extends Controller{
         $articles = course::find($courseId)->articles()->get();
         return response()->json($articles);
     }
+
+    public function login(Request $request){
+        $u = DB::table('members')->where('email',$request->email)->first();
+        if(!$u){
+            $data = [
+                'msg' => "Email tidak terdaftar",
+                'email' => $request->email
+            ];
+            return response()->json($data);
+            
+        }else if ($u->password == $request->password) {
+            $data = [
+                'loggedin' => TRUE,
+                'uid' => $u->id,
+                'email' => $u->email,
+                'admin' => $u->admin,
+                'profilePic' => $u->profilePic
+            ];
+            return response()->json($data);
+        }else{
+            $data = [
+                'msg' => "Maaf, Password yang dimasukkan salah!.",
+                'email' => $request->email
+            ];            
+            return response()->json($data);
+        }
+    }
+
 
 }
